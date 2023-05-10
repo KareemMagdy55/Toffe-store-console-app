e47a427a8077f9628976b20402b51ab8b6b3ddd2
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2023-05-11 00:52:08 +0300

    Test DB's saveCustomers function in main.

--- Toffe_Console_App/src/Main.java
+++ Toffe_Console_App/src/Main.java
@@ -1,11 +1,17 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         DataBase DB = new DataBase();
-        DB.customers.get(3).printCustomer();
-        System.out.println("=======================");
-        DB.products.get(3).printProduct();
+        String str = (DB.customers.get(3).makeString());
+        DB.saveCustomers();
+//        System.out.println("=======================");
+//        DB.products.get(3).printProduct();
+        System.out.print(str);
+
     }
+
+
 }
