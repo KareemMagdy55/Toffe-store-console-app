0b6a111c160af8928713056d67ba8ead64cc4458
sh3boo <101558920+sh3boo@users.noreply.github.com>
2023-05-13 16:00:38 +0300

    Update Main.java

--- Toffe_Console_App/src/Main.java
+++ Toffe_Console_App/src/Main.java
@@ -1,13 +1,24 @@
+/**
+
+Main class for running the ToffeeApp.
+The ToffeeApp class is responsible for starting and running the application.
+This class creates a new instance of ToffeeApp and calls the runApp() method to start the application.
+*/
 import javax.xml.crypto.Data;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ToffeeApp app = new ToffeeApp();
-        app.runApp();
-    }
+    /**
+ * The main method creates a new instance of ToffeeApp and calls the runApp() method to start the application.
+ *
+ * @param args an array of command-line arguments for the application.
+ * @throws IOException if an I/O error occurs.
+ */
+public static void main(String[] args) throws IOException {
+    ToffeeApp app = new ToffeeApp();
+    app.runApp();
+}
 }
