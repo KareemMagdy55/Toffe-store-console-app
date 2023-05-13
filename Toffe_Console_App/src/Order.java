dd69e339e89e7d549b53be82ec3240c5d8b5430d
sh3boo <101558920+sh3boo@users.noreply.github.com>
2023-05-13 16:19:54 +0300

    Update Order.java

--- Toffe_Console_App/src/Order.java
+++ Toffe_Console_App/src/Order.java
@@ -1,14 +1,23 @@
-import java.util.ArrayList;
-import java.util.Date;
-
+/**
+ * Represents an order placed by a customer.
+ */
 public class Order {
-    private final int id;
-    private final int customerID;
-    private final Date date;
-    ArrayList<OrderedProduct> products;
-    private final double totalPrice;
-    private final int status;
+    private final int id; // the unique identifier of the order
+    private final int customerID; // the unique identifier of the customer who placed the order
+    private final Date date; // the date the order was placed
+    ArrayList<OrderedProduct> products; // the list of products ordered
+    private final double totalPrice; // the total price of the order
+    private final int status; // the status of the order (open or closed)
 
+    /**
+     * Constructs a new order object with the given parameters.
+     * @param id the unique identifier of the order
+     * @param customerID the unique identifier of the customer who placed the order
+     * @param date the date the order was placed
+     * @param products the list of products ordered
+     * @param totalPrice the total price of the order
+     * @param status the status of the order (open or closed)
+     */
     public Order(int id, int customerID, Date date, ArrayList<OrderedProduct> products,
                  double totalPrice, int status) {
         this.id = id;
@@ -19,6 +28,10 @@ public class Order {
         this.status = status;
     }
 
+    /**
+     * Returns a string representation of the order.
+     * @return a string representation of the order
+     */
     public String toString() {
         StringBuilder s = new StringBuilder(String.valueOf(id) + '\n' +
                 customerID + '\n' +
@@ -32,6 +45,10 @@ public class Order {
         s.append(status).append("\n");
         return s.toString();
     }
+
+    /**
+     * Prints a summary of the order.
+     */
     public void print(){
         StringBuilder s = new StringBuilder(
                 "Order ID: " + id + '\n' +
@@ -44,6 +61,10 @@ public class Order {
         else s.append("Open Order");
         System.out.println(s);
     }
+
+    /**
+     * Prints a detailed summary of the order, including the ordered items.
+     */
     public void detailedPrint(){
         print();
         System.out.println("Ordered Items in This Order:");
@@ -53,10 +74,19 @@ public class Order {
             System.out.println("_".repeat(50));
         }
     }
+
+    /**
+     * Returns the unique identifier of the customer who placed the order.
+     * @return the unique identifier of the customer who placed the order
+     */
     public int getCustomerID() {
         return customerID;
     }
 
+    /**
+     * Returns the unique identifier of the order.
+     * @return the unique identifier of the order
+     */
     public int getId() {
         return id;
     }
